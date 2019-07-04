4a9dd9b91a32d110b5e308623cc8830c05765e17
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-07-05 04:40:19 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,3 @@
-nama = input(" nama kamu siapa bosque? ")
-umur = input(" umurmu berapa bosque? ")
+nama = (" nama kamu siapa bosque? ")
+umur = (" umurmu berapa bosque? ")
 print(" wiiih salam kenal ya bossque "+ nama +" \n kamu sudah "+ umur +" tahun")
