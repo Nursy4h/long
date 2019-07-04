1c8622a18143cd888fc7e848c811f0e511783cf9
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-07-05 04:30:42 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,4 @@
 print("hello nama saya : Nursyah \n umur sayah :18 \t tapi boong \n agama saya : islam")
 nama = input(" nama kamu siapa bosque???? ")
 umur = input(" umurmu berapa bosque??? ")
-Suka = input(" hehehe nikmat kan rasanya!! ")
-tidak = input (" hehehe sama dong ")
 print(" wiiih salam kenal ya bossque "+ nama +" \n kamu sudah "+ umur +" tahun ")
